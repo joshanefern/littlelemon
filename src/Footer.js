1c7b7faf4bce0e8514761160a1a2a7c5bc667572
Joshane Fernando <fernandojo3122@gmail.com>
2025-06-27 16:49:38 -0400

    setup

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+      <div className="footer-content">
+        <img src="logo-footer.png" alt="Little Lemon" />
+        <div className="footer-links">
+          <a href="/contact">Contact</a>
+          <a href="/privacy">Privacy Policy</a>
+        </div>
+        <p>© 2023 Little Lemon</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
